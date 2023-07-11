dd72fe9093fbcf7207e828fd17b155516c495424
Vzevallos0090 <vzevallos0090@utm.edu.ec>
2023-07-10 22:33:38 -0500

    primer commit

---
+++ app/Controllers/Home.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Controllers;
+// use App\Models
+
+class Home extends BaseController
+{
+    public function index()
+    {
+        return view('welcome_message');
+    } 
+    
+
+    public function prueba ()
+    {
+        echo 'hola esto es una prueba';
+    }
+
+    public function api ()
+    {
+
+
+        $usuarios= array (
+            "usuario"=>"pepe",
+            "nombres"=>"Jose Calderon",
+            "Ciudad"=>"Portoviejo",
+            "Direccion"=>"Los Olivos",
+            "telefono"=>"+593987654321"
+
+        );
+
+
+        return $this->response->setJSON($usuarios);
+
+    }
+
+
+    public function login(){
+
+return view('login');
+    
+    }
+
+
+    public function testbd($cedula)
+    {
+
+        $this->db=\Config\Database::connect();
+        $query=$this->db->query("SELECT idpersonal, cedula, apellido1, apellido2, nombres, genero 
+        FROM esq_datos_personales.personal where cedula='$cedula'  ");
+        $result=$query->getResult();
+        return $this->response->setJSON($result);
+
+
+        // echo "hola";
+    
+    }
+}
